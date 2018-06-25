af4e19699fbdd215d6e5796dd088faf5e0f5a6bf
root <root@localhost.localdomain>
2018-06-25 18:07:58 +0800

    update

--- leetcode/word_frequency.sh
+++ leetcode/word_frequency.sh
@@ -1,21 +1,34 @@
 # Read from the file words.txt and output the word frequency list to stdout.
 #!/bin/bash
 
-words=$(sed -e 's/ / /g; s/ /\n/g' words.txt)
-
+words=`sed -e 's/ /\n/g' words.txt | sort`
+#touch filter_words.txt
+echo $words > words.txt
+exec 0< words.txt
 count=0
-current_word=`cut -d ' ' -f1 ${words}`
+
+#read word
+#
+#while [ $? -eq 0 ]
+#do
+#	echo "$word"
+#	read word
+#done
+
+echo "Done!"
+
+#current_word=`cut -d ' ' -f1 ${words}`
 #touch ./words_counted
 
 
-for word in $words
-do
-	if [ "$current_word" == "$word" ]
-	then
-		count=$[ $count + 1 ]
-	else
-		echo "$current_word $count"
-		count=0
-	fi
-done
+#for word in $words
+#do
+#	if [ "$current_word" == "$word" ]
+#	then
+#		count=$[ $count + 1 ]
+#	else
+#		echo "$current_word $count"
+#		count=0
+#	fi
+#done
 
